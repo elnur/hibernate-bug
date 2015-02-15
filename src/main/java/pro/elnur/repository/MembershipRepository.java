cdeb43304a24680b357d13e54452ba404799ba52
Elnur Abdurrakhimov <elnur@elnur.pro>
2015-02-15 22:49:51 +0300

    Use EntityManager directly

--- src/main/java/pro/elnur/repository/MembershipRepository.java
+++
@@ -1,8 +0,0 @@
-package pro.elnur.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import pro.elnur.model.Membership;
-import pro.elnur.model.MembershipId;
-
-public interface MembershipRepository extends CrudRepository<Membership, MembershipId> {
-}
